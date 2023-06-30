ed74e8c7c14ba3cde5925a1d3bbdb432e952fe96
Manish <manishdhanapal21@gmail.com>
2023-06-30 21:35:50 +0530

    last update

--- src/Body/Footer.js
+++ src/Body/Footer.js
@@ -7,7 +7,6 @@ import tweet from './tweet.svg'
 function Footer() {
   return (
     <div className='footer-container'>
-        <span className='background3'></span>
         <div className='quotes'>
             <h1>We have what you’re looking for</h1>
             <p className='footer-des'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type a</p>
